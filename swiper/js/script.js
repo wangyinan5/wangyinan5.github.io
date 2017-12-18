b7a68dabc6ad4ced18ca29659c885874c895b1dc
wangyinan5 <w18333625212@163.com>
2017-12-18 14:46:36 +0800

    第一次

---
+++ swiper/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lenovo on 17/12/5 005.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+
+    },
+    on:{
+      init: function(){
+        swiperAnimateCache(this); //隐藏动画元素 
+        swiperAnimate(this); //初始化完成开始动画
+      }, 
+      slideChangeTransitionEnd: function(){ 
+        swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+      } 
+    }
+
+});
+
+
